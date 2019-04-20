71cb5a29c84eb238d1d715135ade37b53ae0e757
Toshiyuki Hirooka <toshi.hirooka@gmail.com>
2019-04-20 13:43:40 +0900

    SDL main func for Cellular Automata

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,43 @@
-int main(int argc, char *argv[]) { return 0; }
+#include <SDL2/SDL.h>
+#include <SDL2pp/SDL2pp.hh>
+
+const std::string title = "Genelife";
+constexpr int pixel_size = 9;
+constexpr int board_width = 64 * 1;
+constexpr int board_height = 64 * 1;
+
+int main(int argc, char *argv[]) {
+  SDL2pp::SDL sdl(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER |
+                  SDL_INIT_EVENTS);
+  SDL2pp::Window window(title, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                        board_width * pixel_size, board_height * pixel_size, 0);
+  SDL2pp::Renderer renderer(
+      window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+  while (true) {
+    SDL_Event event;
+    SDL_PollEvent(&event);
+    if (event.type == SDL_QUIT)
+      return 0;
+    if (event.type == SDL_KEYDOWN) {
+      if (event.key.keysym.sym == SDLK_ESCAPE)
+        return 0;
+    }
+
+    renderer.SetDrawColor(0, 0, 0, 255);
+    renderer.Clear();
+    for (int y = 0; y < board_height; y++) {
+      for (int x = 0; x < board_width; x++) {
+        {
+          renderer.SetDrawColor(255, 255, 255, 255);
+          renderer.FillRect(x * pixel_size, y * pixel_size,
+                            x * pixel_size + pixel_size - 1,
+                            y * pixel_size + pixel_size - 1);
+        }
+      }
+    }
+    renderer.Present();
+  }
+
+  return 0;
+}
